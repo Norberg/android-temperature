89ec8aaa0517a10161fbf3407039c5b07f5bee02
Simon Norberg <simon@pthread.se>
2011-12-31 19:52:08 +0100

    cleaned up the code a bit

--- temperature.py
+++ temperature.py
@@ -2,6 +2,30 @@ import android
 import urllib
 droid=android.Android()
 
+layout="""<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+	android:id="@+id/background"
+	android:orientation="vertical" android:layout_width="match_parent"
+	android:layout_height="match_parent" android:background="#ff000000">
+	<LinearLayout android:layout_width="match_parent"
+		android:layout_height="wrap_content" android:id="@+id/linearLayout1">
+			<Button android:id="@+id/button_daily" android:layout_width="wrap_content"
+				android:layout_height="wrap_content" android:text="Daily"></Button>
+			<Button android:id="@+id/button_weekly" android:layout_width="wrap_content"
+				android:layout_height="wrap_content" android:text="Weekly"></Button>
+			<Button android:id="@+id/button_monthly" android:layout_width="wrap_content"
+				android:layout_height="wrap_content" android:text="Monthly"></Button>
+			<Button android:id="@+id/button_monthly_avg" android:layout_width="wrap_content"
+				android:layout_height="wrap_content" android:text="Monthly Average"></Button>
+	</LinearLayout>
+	<ImageView android:id="@+id/imageview"
+		android:src="@drawable/brush"
+		android:layout_width="wrap_content"
+		android:layout_height="wrap_content" />
+	<Button android:id="@+id/button_quit" android:layout_width="wrap_content"
+		android:layout_height="wrap_content" android:text="Quit"></Button>
+</LinearLayout>
+"""
 def eventloop():
 	while True:
 		event=droid.eventWait().result
@@ -27,30 +51,6 @@ def change_weather(timeframe):
 	droid.fullSetProperty("imageview","src","file:///sdcard/download/%s.png" % timeframe)
 	
 def main():
-	layout="""<?xml version="1.0" encoding="utf-8"?>
-	<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-		android:id="@+id/background"
-		android:orientation="vertical" android:layout_width="match_parent"
-		android:layout_height="match_parent" android:background="#ff000000">
-		<LinearLayout android:layout_width="match_parent"
-			android:layout_height="wrap_content" android:id="@+id/linearLayout1">
-				<Button android:id="@+id/button_daily" android:layout_width="wrap_content"
-					android:layout_height="wrap_content" android:text="Daily"></Button>
-				<Button android:id="@+id/button_weekly" android:layout_width="wrap_content"
-					android:layout_height="wrap_content" android:text="Weekly"></Button>
-				<Button android:id="@+id/button_monthly" android:layout_width="wrap_content"
-					android:layout_height="wrap_content" android:text="Monthly"></Button>
-				<Button android:id="@+id/button_monthly_avg" android:layout_width="wrap_content"
-					android:layout_height="wrap_content" android:text="Monthly Average"></Button>
-		</LinearLayout>
-		<ImageView android:id="@+id/imageview"
-			android:src="@drawable/brush"
-			android:layout_width="wrap_content"
-			android:layout_height="wrap_content" />
-		<Button android:id="@+id/button_quit" android:layout_width="wrap_content"
-			android:layout_height="wrap_content" android:text="Quit"></Button>
-	</LinearLayout>
-	"""
 	print droid.fullShow(layout)
 	eventloop()
 	droid.fullDismiss()
